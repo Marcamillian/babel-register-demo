f093d62bdf294e459356197a3c4cb16b3a05c2a0
Marcamillian <marcamillian@gmail.com>
2019-04-16 18:36:46 +0100

    feat: webpack running build
    package.json
    - add browser-sync
    - add webpack
    - add webpack middleware
    
    tasks/index.js
    - require gulp / scripts / server
    - export the gulp build task (webpack scripts)
    
    server.js
    - added to define how to use browser-sync
    
    webpack.js
    - define the scripts that run webpack

--- tasks/index.js
+++ tasks/index.js
@@ -2,8 +2,14 @@ require('@babel/register');
 
 const subtask = require('./subtask').default;
 
+const gulp = require('gulp')
+const { scripts } = require('./webpack')
+const { server } = require('./server')
+
 exports.default = (cb)=>{
   console.log("loaded index")
   subtask()
   cb()
 }
+
+exports.build = gulp.series( scripts )
---
+++ tasks/webpack.js
@@ -0,0 +1,24 @@
+import path from 'path'
+import webpack from 'webpack'
+
+let config = {
+  entry: '../react/main.js',
+  output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname, '../site')
+  },
+  context: path.resolve(__dirname, '../site')
+}
+
+function scripts(){
+  return new Promise(resolve => webpack(config, (err, stats)=>{
+
+    if(err) console.log('Webpack', err)
+
+    console.log(stats.toString({/* stats options */}))
+
+    resolve()
+  }))
+}
+
+module.exports = { config, scripts }
